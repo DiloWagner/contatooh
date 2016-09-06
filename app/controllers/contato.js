5d056462c99cb0f9a41138878d3c4cd814362861
DiloWagner <diegowagner4@gmail.com>
2016-09-05 22:54:25 -0300

    Ajuste nos dados do controller

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,8 +1,7 @@
 var contatos = [
     {_id: 1,nome: 'Contato Exemplo 1',email: 'cont1@empresa.com.br'},
     {_id: 2,nome: 'Contato Exemplo 2',email: 'cont2@empresa.com.br'},
-    {_id: 3,nome: 'Contato Exemplo 3',email: 'cont3@empresa.com.br'},
-    {_id: 4,nome: 'Diego Wagner',email: 'diegowagner4@gmail.com'}
+    {_id: 3,nome: 'Contato Exemplo 3',email: 'cont3@empresa.com.br'}
 ];
 
 const ID_CONTATO_INC = 4;
