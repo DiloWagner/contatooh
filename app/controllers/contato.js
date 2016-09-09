526f645b62e895a9c8570dd9909e7563bfeef79d
DiloWagner <diegowagner4@gmail.com>
2016-09-08 22:41:31 -0300

    Removendo array de contatos - não será mais utilizado

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,11 +1,3 @@
-var contatos = [
-    {_id: 1,nome: 'Contato Exemplo 1',email: 'cont1@empresa.com.br'},
-    {_id: 2,nome: 'Contato Exemplo 2',email: 'cont2@empresa.com.br'},
-    {_id: 3,nome: 'Contato Exemplo 3',email: 'cont3@empresa.com.br'}
-];
-
-const ID_CONTATO_INC = 3;
-
 module.exports = function(app) {
 
     var Contato = app.models.contato;
